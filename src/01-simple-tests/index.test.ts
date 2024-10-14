90724af34aa47beeea03b977d92386e072f69b55
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-10-14 20:26:02 +0200

    feat: add simple, table, async, error tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,79 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { Action, simpleCalculator } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
     // Write your test here
+    const result = simpleCalculator({ a: 2, b: 2, action: Action.Add });
+
+    expect(result).toBe(4);
   });
 
   test('should subtract two numbers', () => {
     // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Subtract });
+
+    expect(result).toBe(4);
   });
 
   test('should multiply two numbers', () => {
     // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Multiply });
+
+    expect(result).toBe(12);
   });
 
   test('should divide two numbers', () => {
     // Write your test here
+    const result = simpleCalculator({ a: 6, b: 2, action: Action.Divide });
+
+    expect(result).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
     // Write your test here
+    const result = simpleCalculator({
+      a: 5,
+      b: 2,
+      action: Action.Exponentiate,
+    });
+
+    expect(result).toBe(25);
   });
 
   test('should return null for invalid action', () => {
     // Write your test here
+    const result = simpleCalculator({
+      a: 3,
+      b: 2,
+      action: 'Divid',
+    });
+
+    expect(result).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
     // Write your test here
+    const firstResult = simpleCalculator({
+      a: '3',
+      b: 2,
+      action: Action.Add,
+    });
+
+    const secondResult = simpleCalculator({
+      a: 3,
+      b: '2',
+      action: Action.Add,
+    });
+
+    const thirdResult = simpleCalculator({
+      a: '3',
+      b: '2',
+      action: Action.Add,
+    });
+
+    expect(firstResult).toBe(null);
+    expect(secondResult).toBe(null);
+    expect(thirdResult).toBe(null);
   });
 });
--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,47 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  resolveValue,
+  throwCustomError,
+  throwError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
     // Write your test here
+    expect(await resolveValue(5)).toEqual(5);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     // Write your test here
+    const ERROR = 'Custom error';
+
+    expect(() => throwError(ERROR)).toThrow(ERROR);
   });
 
   test('should throw error with default message if message is not provided', () => {
     // Write your test here
+    const DEFAULT_ERROR = 'Oops!';
+    expect(() => throwError()).toThrow(DEFAULT_ERROR);
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
     // Write your test here
+    expect(() => {
+      throwCustomError();
+    }).toThrow('This is my awesome custom error!');
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
     // Write your test here
+    await expect(rejectCustomError()).rejects.toThrow(
+      'This is my awesome custom error!',
+    );
   });
 });
