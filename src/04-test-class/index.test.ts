3bab8f2b36850cbd3c166af1c690ff7963158600
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2024-10-15 22:55:25 +0200

    feat: add class tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,62 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount } from '.';
 
 describe('BankAccount', () => {
+  const BALANCE = 250;
+
+  const UserAccount = getBankAccount(BALANCE);
   test('should create account with initial balance', () => {
     // Write your test here
+
+    expect(UserAccount.getBalance()).toBe(BALANCE);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
     // Write your test here
+    expect(() => UserAccount.withdraw(300)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${BALANCE}`,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
+    const SecondAccount = getBankAccount(200);
+
     // Write your test here
+    expect(() => UserAccount.transfer(300, SecondAccount)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${BALANCE}`,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
     // Write your test here
+    expect(() => UserAccount.transfer(100, UserAccount)).toThrow(
+      'Transfer failed',
+    );
   });
 
   test('should deposit money', () => {
     // Write your test here
+    UserAccount.deposit(BALANCE);
+
+    expect(UserAccount.getBalance()).toBe(500);
   });
 
   test('should withdraw money', () => {
     // Write your test here
+
+    UserAccount.withdraw(100);
+
+    expect(UserAccount.getBalance()).toBe(150);
   });
 
   test('should transfer money', () => {
     // Write your test here
+
+    const SecondAccount = getBankAccount(200);
+
+    UserAccount.transfer(100, SecondAccount);
+
+    expect(SecondAccount.getBalance()).toBe(300);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
